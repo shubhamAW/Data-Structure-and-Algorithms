587792d5198a97fe2cad25cd16bd2a7bfbceb7dc
shubhamAW <waykarshubham2022@gmail.com>
2023-04-14 14:38:37 +0530

    added Linked List operation-completed

--- Day03/1.cpp
+++ Day03/1.cpp
@@ -4,6 +4,7 @@ using namespace std;
 // class Employee
 class Employee
 {
+public:
     int id;
     string name;
 
@@ -12,46 +13,56 @@ class Employee
         this->id = id;
         this->name = name;
     }
+    Employee()
+    {
+        this->id = 0;
+        this->name = "";
+    }
 };
 
-// linked list.
 struct Node
 {
-    int data;
+    Employee data;
     Node *next;
+
+    Node(Employee data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
 };
 
 /// @brief Add New Node to at the End of List
 /// @param head
 /// @param tail
 /// @param data
-void AddToTail(Node **head, Node **tail, int data)
+void AddToTail(Node **head, Node **tail, Employee data)
 {
-    Node *temp = new Node;
-    temp->data = data;
-    temp->next = NULL;
+    cout << "Tempararly disabled" << endl;
+    // Node *temp = new Node(data);
 
-    if (*head == NULL)
-    {
-        *head = temp;
-        *tail = temp;
-    }
-    else
-    {
-        (*tail)->next = temp;
-        *tail = temp;
-    }
+    // if (*head == NULL)
+    // {
+    //     *head = temp;
+    //     *tail = temp;
+    // }
+    // else
+    // {
+    //     (*tail)->next = temp;
+    //     *tail = temp;
+    // }
 }
 
 /// @brief Add New Node to Start of the List
 /// @param head
 /// @param data
-void AddToStart(Node **head, int data)
+void AddToStart(Node **head, Employee data)
 {
-    Node *temp = new Node;
-    temp->data = data;
-    temp->next = *head;
-    *head = temp;
+    cout << "Tempararly disabled" << endl;
+    // Node *temp = new Node(data);
+    // temp->data = data;
+    // temp->next = *head;
+    // *head = temp;
 }
 
 /// @brief Print the List from head
@@ -61,7 +72,7 @@ void printList(Node *head)
     Node *temp = head;
     while (temp != NULL)
     {
-        cout << temp->data << " ";
+        cout << temp->data.id << " " << temp->data.name << " ";
         temp = temp->next;
     }
     cout << endl;
@@ -71,7 +82,7 @@ void printList(Node *head)
 /// @param head
 /// @param data
 /// @return return true if it is found, return false if not found.
-bool SearchList(Node *head, int data)
+bool SearchList(Node *head, Employee data)
 {
     if (head == NULL)
     {
@@ -80,7 +91,7 @@ bool SearchList(Node *head, int data)
     Node *temp = head;
     while (temp != NULL)
     {
-        if (temp->data == data)
+        if (temp->data.id == data.id)
         {
             return true;
         }
@@ -93,26 +104,42 @@ bool SearchList(Node *head, int data)
 /// @param head
 /// @param data
 /// @param position
-void InsertAtPosition(Node **head, int data, int position)
+void InsertAtPosition(Node **head, Employee data)
 {
-    Node *temp = new Node;
-    temp->data = data;
-    temp->next = NULL;
-
-    if (position == 0)
+    Node *temp = new Node(data);
+    // first check if the id is already present in the list
+    if (SearchList(*head, data))
     {
-        temp->next = *head;
-        *head = temp;
+        cout << "ID already present in the list" << endl;
+        return;
     }
     else
     {
-        Node *temp2 = *head;
-        for (int i = 0; i < position - 1; i++)
+        if (*head == NULL)
         {
-            temp2 = temp2->next;
+            *head = temp;
         }
-        temp->next = temp2->next;
-        temp2->next = temp;
+        else
+        {
+            int key = data.id;
+            Node *temp2 = *head;
+            Node *prev = NULL;
+            while (temp2 != NULL && temp2->data.id < key)
+            {
+                prev = temp2;
+                temp2 = temp2->next;
+            }
+            if (prev == NULL)
+            {
+                temp->next = *head;
+                *head = temp;
+            }
+            else
+            {
+                temp->next = prev->next;
+                prev->next = temp;
+            }
+        }
     }
 }
 
@@ -120,29 +147,34 @@ void InsertAtPosition(Node **head, int data, int position)
 /// @param head
 /// @param data
 /// @param position
-void DeletePosition(Node **head, int position)
+void DeleteAtPosition(Node **head, int id)
 {
     if (*head == NULL)
     {
         return;
     }
-
-    if (position == 0)
+    // if the id is not present in the list
+    Node *temp = *head;
+    Node *prev = NULL;
+    while (temp != NULL && temp->data.id != id)
     {
-        Node *temp = *head;
-        *head = (*head)->next;
+        prev = temp;
+        temp = temp->next;
+    }
+    if (temp == NULL)
+    {
+        cout << "ID not present in the list" << endl;
+        return;
+    }
+    if (prev == NULL)
+    {
+        *head = temp->next;
         delete temp;
     }
     else
     {
-        Node *temp = *head;
-        for (int i = 0; i < position - 1; i++)
-        {
-            temp = temp->next;
-        }
-        Node *temp2 = temp->next;
-        temp->next = temp2->next;
-        delete temp2;
+        prev->next = temp->next;
+        delete temp;
     }
 }
 
@@ -156,14 +188,16 @@ int main()
 
     int position;
     int choice;
+    string name;
+
     do
     {
         cout << "1. Add to tail." << endl;
         cout << "2. Add to start." << endl;
         cout << "3. Search." << endl;
-        cout << "4. Insert at specific  position." << endl;
+        cout << "4. Insert in List using id." << endl;
         cout << "5. Print List." << endl;
-        cout << "6.Delete Node at specific Position." << endl;
+        cout << "6.Delete Node using id." << endl;
         cout << "7. Exit" << endl;
         cout << "Enter your choice: ";
         cin >> choice;
@@ -171,20 +205,28 @@ int main()
         switch (choice)
         {
         case 1:
+            cout << "Tempararly disabled" << endl;
             int data;
-            cout << "Enter data: ";
-            cin >> data;
-            AddToTail(&head, &tail, data);
+            // cout << "Enter data: ";
+            // cin >> data;
+            // cout << "Enter a Name: ";
+            // cin >> name;
+            // AddToTail(&head, &tail, Employee(data, name));
             break;
         case 2:
-            cout << "Enter data: ";
-            cin >> data;
-            AddToStart(&head, data);
+            cout << "Tempararly disabled" << endl;
+            // cout << "Enter data: ";
+            // cin >> data;
+            // cout << "Enter a Name: ";
+            // cin >> name;
+            // AddToStart(&head, Employee(data, name));
             break;
         case 3:
             cout << "Enter data: ";
             cin >> data;
-            if (SearchList(head, data))
+            cout << "Enter a Name: ";
+            cin >> name;
+            if (SearchList(head, Employee(data, name)))
             {
                 cout << "Found" << endl;
             }
@@ -196,17 +238,18 @@ int main()
         case 4:
             cout << "Enter data: ";
             cin >> data;
-            cout << "Enter position: ";
-            cin >> position;
-            InsertAtPosition(&head, data, position);
+            cout << "Enter a Name: ";
+            cin >> name;
+            InsertAtPosition(&head, Employee(data, name));
             break;
         case 5:
             printList(head);
             break;
         case 6:
-            cout << "Enter Position: ";
-            cin >> position;
-            DeletePosition(&head, position);
+            int id;
+            cout << "Enter id: ";
+            cin >> id;
+            DeleteAtPosition(&head, id);
             break;
         case 7:
             cout << "Exit" << endl;
